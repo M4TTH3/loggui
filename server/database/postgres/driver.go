592ccea08d5f3cec573444749a0861800ba3dd74
M4TTH3 <matthewau-yeung@hotmail.com>
2025-05-26 01:16:15 -0400

    add test GH action

--- server/database/postgres/driver.go
+++ server/database/postgres/driver.go
@@ -3,11 +3,25 @@ package postgres
 import (
 	"context"
 	"github.com/jackc/pgx/v5"
+	"github.com/m4tth3/loggui/core"
 	d "github.com/m4tth3/loggui/server/database"
 )
 
 type driver struct {
 	conn *pgx.Conn
+}
+
+func (d driver) Init() error {
+	// Initialize the connection or perform any setup needed.
+	return nil
+}
+
+func (d driver) GetLogs(filter *d.Filter) (chan *core.Log, error) {
+	return make(chan *core.Log), nil
+}
+
+func (d driver) WriteLog(log *core.Log) error {
+	return nil
 }
 
 func NewQueryHandler(url string) (d.QueryHandler, error) {
